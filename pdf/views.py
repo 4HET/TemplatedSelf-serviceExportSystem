a9cf5b5a6da0e7c323790607a55e1077131b421a
4HET <2659335014@qq.com>
2022-10-14 11:44:19 +0800

    export zxqy finish completely

--- pdf/views.py
+++ pdf/views.py
@@ -1,10 +1,13 @@
 import time
 import traceback
 
+from django.http import StreamingHttpResponse
 from django.shortcuts import render, redirect
+from django.utils.encoding import escape_uri_path
 from docx2pdf import convert
 
 import pythoncom
+
 
 # Create your views here.
 def pdf(request):
@@ -14,6 +17,7 @@ def pdf(request):
     if not status:
         return redirect('/login/')
     return render(request, 'pdf.html', ctx)
+
 
 def xywjpdf(request):
     ctx = {}
@@ -39,6 +43,7 @@ def xywjpdf(request):
         return redirect('/third')
     return render(request, 'pdf.html', ctx)
 
+
 def zxqypdf(request):
     ctx = {}
     username = request.COOKIES.get("username")
@@ -55,6 +60,23 @@ def zxqypdf(request):
         file.close()
 
         convert(inputFile, outputFile)
+
+        def down_chunk_file_manager(file_path, chuck_size=1024):
+            with open(file_path, "rb") as file:
+                while True:
+                    chuck_stream = file.read(chuck_size)
+                    if chuck_stream:
+                        yield chuck_stream
+                    else:
+                        break
+
+        response = StreamingHttpResponse(down_chunk_file_manager(outputFile))
+        response['Content-Type'] = 'application/octet-stream'
+        the_file_name = "中小企业声明函.pdf"
+        # response["Content-Disposition"] = "attachment; filename*=UTF-8''{}".format(escape_uri_path(filename))
+        response["Content-Disposition"] = "attachment; filename*=UTF-8''{}".format(escape_uri_path(the_file_name))
+
+        return response
     except Exception as e:
         print(traceback.format_exc())
         return redirect('/third')
